33b86b9eba593335269ba44324ac68d82ac44ea3
Adan Rosales <adan.rosales_c@janrain.com>
2019-08-02 00:02:12 -0500

    Add ApprovalCard and CommentDetail components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './commentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const comments=[
+    {'author':faker.name.firstName(), 'comment':faker.hacker.phrase(), 'date':faker.date.past(),'avatar':faker.image.avatar()},
+    {'author':faker.name.firstName(),  'comment':faker.hacker.phrase(),'date':faker.date.past(),'avatar':faker.image.avatar()},
+    {'author':faker.name.firstName(),  'comment':faker.hacker.ingverb(),'date':faker.date.past(),'avatar':faker.image.avatar()},
+    {'author':faker.name.firstName(),  'comment':faker.hacker.ingverb(),'date':faker.date.past(),'avatar':faker.image.avatar()},
+    {'author':faker.name.firstName(),  'comment':faker.hacker.phrase(),'date':faker.date.past(),'avatar':faker.image.avatar()},
+    {'author':faker.name.firstName(),  'comment':faker.hacker.ingverb(),'date':faker.date.past(),'avatar':faker.image.avatar()}
+];
+
+const App = () =>{
+    const allcomments = comments.map(item=>{
+        return(
+            <ApprovalCard >
+                <CommentDetail 
+                    author ={item.author} 
+                    comment = {item.comment} 
+                    avatar = {item.avatar}
+                    date = {item.date}/>
+            </ApprovalCard>
+        );
+    })
+    return (
+    <div className="ui container comments">
+    <br/>
+      {allcomments}
+    </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
